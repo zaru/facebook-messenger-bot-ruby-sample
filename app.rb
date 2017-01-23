0f243a86af4ee6cb2fc9ed102249e4c47f25eaa8
zaru <sakuraba@basicinc.jp>
2017-01-23 14:58:58 +0900

    add sinatra reloader

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 configure { set :server, :puma }
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get "/" do
     "Hoge"
   end
